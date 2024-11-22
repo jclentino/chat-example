6d7c04749e7f2d9c34d3352c32a003909d0039c6
jclentino <nuncaparesdecrear@gmail.com>
2024-11-22 02:11:25 -0500

    line, data y close events

--- client.js
+++ client.js
@@ -11,3 +11,15 @@ const client = new net.Socket()
 client.connect(3000, '127.0.0.1', () => {
   console.log('Conectado al servidor...')
 })
+
+rl.on('line', (line) => {
+  client.write(line)
+})
+
+client.on('data', (data) => {
+  console.log(data.toString())
+})
+
+client.on('close', () => {
+  rl.close()
+})
